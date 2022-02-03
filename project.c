4887fd8a96eefec5145ff1dc6396a67c85158b98
Waldemar Faist <cubic@coldice.net>
2022-02-03 15:12:30 +0100

    fix reverted version to command handler

--- project.c
+++ project.c
@@ -7,19 +7,19 @@
 #include "dev/serial-line.h"
 #include <stdio.h>
 
-PROCESS(button_handler, "Handle button");
-AUTOSTART_PROCESSES(&button_handler);
+PROCESS(command_handler, "Handle button");
+AUTOSTART_PROCESSES(&command_handler);
 
-PROCESS_THREAD(button_handler, ev, data) {
+PROCESS_THREAD(command_handler, ev, data) {
     PROCESS_BEGIN();
 
     uart1_set_input(serial_line_input_byte);
     serial_line_init();
 
     for(;;) {
-        PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event && data == &button_sensor);
+        PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message && data != NULL);
 
-        printf("Button pressed!\n");
+        printf("received command: '%s'\n", (const char*)data);
     }
 
     PROCESS_END();
