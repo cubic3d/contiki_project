3ee2e139a1a96e97bd691597bd06b8320f7a661d
Waldemar Faist <cubic@coldice.net>
2022-02-03 01:10:16 +0100

    drop serial commands

--- project.c
+++ project.c
@@ -3,21 +3,20 @@
 #include "random.h"
 #include "dev/button-sensor.h"
 #include "dev/leds.h"
-#include "dev/serial-line.h"
 #include <stdio.h>
 
-PROCESS(command_receiver, "Receive commands over serial");
-AUTOSTART_PROCESSES(&command_receiver);
+PROCESS(button_handler, "Handle button");
+AUTOSTART_PROCESSES(&button_handler);
 
-PROCESS_THREAD(command_receiver, ev, data) {
+PROCESS_THREAD(button_handler, ev, data) {
     PROCESS_BEGIN();
 
-    for(;;) {
-        PROCESS_WAIT_EVENT();
+    SENSORS_ACTIVATE(button_sensor);
 
-        if(ev == serial_line_event_message && data != NULL) {
-            printf("received command: '%s'\n", (const char*)data);
-        }
+    for(;;) {
+        PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event && data == &button_sensor);
+
+        printf("Button pressed!\n");
     }
 
     PROCESS_END();
