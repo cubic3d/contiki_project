d31bde560ed55767b15108cd06b42a4913b2f90a
Waldemar Faist <cubic@coldice.net>
2022-02-03 15:53:45 +0100

    skelleton processing commands

--- project.c
+++ project.c
@@ -7,19 +7,27 @@
 #include "dev/serial-line.h"
 #include <stdio.h>
 
-PROCESS(command_handler, "Handle button");
+PROCESS(command_handler, "Handle commands");
 AUTOSTART_PROCESSES(&command_handler);
 
 PROCESS_THREAD(command_handler, ev, data) {
     PROCESS_BEGIN();
 
+    // Hook UART1 to serial line API
     uart1_set_input(serial_line_input_byte);
     serial_line_init();
 
     for(;;) {
         PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message && data != NULL);
 
-        printf("received command: '%s'\n", (const char*)data);
+        // Extract command by separating received line by " "
+        char *command = strtok((char*)data, " ");
+
+        if(strcmp(command, "rreq") == 0) {
+            // Send RREQ to node with specified ID
+            char *id = strtok(NULL, " ");
+            printf("Sending RREQ to %s\n", id);
+        }
     }
 
     PROCESS_END();
