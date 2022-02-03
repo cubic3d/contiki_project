8f337d0157244b50ac40ff208e46f735fcdd3a49
Waldemar Faist <cubic@coldice.net>
2022-02-03 14:49:12 +0100

    revert again, yay it uses uart1

--- project.c
+++ project.c
@@ -3,6 +3,7 @@
 #include "random.h"
 #include "dev/button-sensor.h"
 #include "dev/leds.h"
+#include "dev/uart1.h"
 #include "dev/serial-line.h"
 #include <stdio.h>
 
@@ -11,6 +12,9 @@ AUTOSTART_PROCESSES(&command_receiver);
 
 PROCESS_THREAD(command_receiver, ev, data) {
     PROCESS_BEGIN();
+
+    uart1_set_input(serial_line_input_byte);
+    serial_line_init();
 
     for(;;) {
         PROCESS_WAIT_EVENT();
