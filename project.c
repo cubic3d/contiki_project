8df0471d08a27230eb281e858b7f38af46ed6d1c
Waldemar Faist <cubic@coldice.net>
2022-02-04 22:25:18 +0100

    create unicast connection on new channel

--- project.c
+++ project.c
@@ -17,6 +17,7 @@ the datatype, since it's sufficient as a demonstration.
 
 
 static struct broadcast_conn broadcast;
+static struct unicast_conn unicast;
 
 // Handle incomming broadcast packets.
 static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from) {
@@ -49,12 +50,30 @@ static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from) {
 }
 static const struct broadcast_callbacks broadcast_cb = {broadcast_recv};
 
+// Handle incomming unicast packets.
+static void
+unicast_recv(struct unicast_conn *c, const linkaddr_t *from) {
+    static uint8_t *data;
+    data = (uint8_t *)packetbuf_dataptr();
+
+    // Handle packet depending on its type
+    switch(*data) {
+        case RREQ:
+            break;
+
+        default:
+            printf("Received unknown packet type %d", data[0]);
+    }
+}
+static const struct unicast_callbacks unicast_cb = {unicast_recv};
+
 
 PROCESS(init, "Main process and command handler");
 AUTOSTART_PROCESSES(&init);
 
 PROCESS_THREAD(init, ev, data) {
     PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+    PROCESS_EXITHANDLER(unicast_close(&unicast);)
 
     PROCESS_BEGIN();
 
@@ -65,6 +84,7 @@ PROCESS_THREAD(init, ev, data) {
     serial_line_init();
 
     broadcast_open(&broadcast, 129, &broadcast_cb);
+    unicast_open(&unicast, 146, &unicast_cb);
 
     for(;;) {
         PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message && data != NULL);
